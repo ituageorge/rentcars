f997fa868193adb0d5fe9dedf050fcdacd2dae22
ituaosagie100@gmail.com <ituaosagie100@gmail.com>
2022-12-02 06:49:18 -0800

    react set up and route configuration

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BookYourCar from './pages/BookYourCar';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+<Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/login' exact element={<Login/>} />
+          <Route path='/register' exact element={<Register/>} />
+          <Route path='/bookyourcar' exact element={<BookYourCar/>} />
+</Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
