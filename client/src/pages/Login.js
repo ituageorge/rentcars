ecfc5d47032a1f5b8d570e68f5b66360e39ac64a
ituaosagie100@gmail.com <ituaosagie100@gmail.com>
2022-12-07 12:00:37 -0800

    login and register page design

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {Link } from 'react-router-dom'
+import {Row, Col, Form, Input} from 'antd'
 
 function Login() {
     return(
-        <div>
-            <h1>Login Page</h1>
+        <div className='login'>
+          <Row gutter={16} className='d-flex align-items-center'>
+                <Col lg={16} style={{position:'relative'}}>
+                    <img src='https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' />
+                    <h1 className='login-logo'>GeorgeMotors</h1>
+                </Col>
+                <Col lg={8} className='text-left p-5'>
+                    <Form layout='vertical' className='login-form p-5'>
+                        <h1>Login</h1>
+                        <hr/>
+                        <Form.Item name='username' label='Username' rules={[{required: true}]}>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item name='password' label='Password' rules={[{required: true}]}>
+                            <Input/>
+                        </Form.Item>
+                        <button className='btn1 mt-2 mb-2'>Login</button>
+                        <br />
+                        <Link to='/register'>Click here to register </Link>
+                    </Form>
+                </Col>
+
+          </Row>
         </div>
     )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import {Row, Col, Form, Input} from 'antd'
+import {Link} from 'react-router-dom'
 
 function Register() {
     return(
-        <div>
-            <h1>Register Page</h1>
+        <div className='login'>
+          <Row gutter={16} className='d-flex align-items-center'>
+                <Col lg={16} style={{position:'relative'}}>
+                    <img src='https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?w=740&t=st=1670439611~exp=1670440211~hmac=f47db0d5996ac3591f0e7f6bc585cc8319af251e326a6eceb00c4e28957b4406' />
+                    <h1 className='login-logo'>GeorgeMotors</h1>
+                </Col>
+                <Col lg={8} className='text-left p-5'>
+                    <Form layout='vertical' className='login-form p-5'>
+                        <h1>Register</h1>
+                        <hr/>
+                        <Form.Item name='username' label='Username' rules={[{required: true}]}>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item name='password' label='Password' rules={[{required: true}]}>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item name='password' label='Password' rules={[{required: true}]}>
+                            <Input/>
+                        </Form.Item>
+                        <button className='btn1 mt-2 mb-3'>Register</button>
+                            <br />
+                        <Link to='/login'>Click here to log in</Link>
+                    </Form>
+                </Col>
+
+          </Row>
         </div>
     )
 }
