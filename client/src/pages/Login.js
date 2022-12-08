44ee97dfb8acad74d8df2b8201b4fe52f5d0dcb3
ituaosagie100@gmail.com <ituaosagie100@gmail.com>
2022-12-08 00:19:13 -0800

    login and register actions

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import {Link } from 'react-router-dom'
 import {Row, Col, Form, Input} from 'antd'
+import {useDispatch} from 'react-redux'
+import { userLogin } from '../redux/actions/userAction'
 
 function Login() {
+    const dispatch = useDispatch()
+    function onFinish(values) {
+        dispatch(userLogin(values))
+        console.log(values)
+       }
+
     return(
         <div className='login'>
           <Row gutter={16} className='d-flex align-items-center'>
@@ -11,7 +19,7 @@ function Login() {
                     <h1 className='login-logo'>GeorgeMotors</h1>
                 </Col>
                 <Col lg={8} className='text-left p-5'>
-                    <Form layout='vertical' className='login-form p-5'>
+                    <Form layout='vertical' className='login-form p-5' onFinish={onFinish}>
                         <h1>Login</h1>
                         <hr/>
                         <Form.Item name='username' label='Username' rules={[{required: true}]}>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import {Row, Col, Form, Input} from 'antd'
 import {Link} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
+import { userRegister } from '../redux/actions/userAction'
+
 
 function Register() {
+    const dispatch = useDispatch() 
+   function onFinish(values) {
+    dispatch(userRegister(values))
+    console.log(values)
+   }
+
     return(
         <div className='login'>
           <Row gutter={16} className='d-flex align-items-center'>
@@ -11,7 +20,7 @@ function Register() {
                     <h1 className='login-logo'>GeorgeMotors</h1>
                 </Col>
                 <Col lg={8} className='text-left p-5'>
-                    <Form layout='vertical' className='login-form p-5'>
+                    <Form layout='vertical' className='login-form p-5' onFinish={onFinish}>
                         <h1>Register</h1>
                         <hr/>
                         <Form.Item name='username' label='Username' rules={[{required: true}]}>
