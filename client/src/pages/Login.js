7e0580e0f5499b98c9e03211ad22f46ba12aa3e6
ituaosagie100@gmail.com <ituaosagie100@gmail.com>
2022-12-08 10:06:48 -0800

    car image animation in login and register page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Link } from 'react-router-dom'
 import {Row, Col, Form, Input} from 'antd'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { userLogin } from '../redux/actions/userAction'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import Spinner from '../components/Spinner'
+// ..
+AOS.init();
 
 function Login() {
     const dispatch = useDispatch()
+    const {loading} = useSelector(state=>state.alertsReducer)
     function onFinish(values) {
         dispatch(userLogin(values))
         console.log(values)
@@ -13,9 +19,15 @@ function Login() {
 
     return(
         <div className='login'>
+            {loading && (<Spinner />)} 
+                
+           
           <Row gutter={16} className='d-flex align-items-center'>
                 <Col lg={16} style={{position:'relative'}}>
-                    <img src='https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' />
+                    <img 
+                    data-aos='slide-right'
+                    data-aos-duration='1500'
+                    src='https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' />
                     <h1 className='login-logo'>GeorgeMotors</h1>
                 </Col>
                 <Col lg={8} className='text-left p-5'>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import {Row, Col, Form, Input} from 'antd'
 import {Link} from 'react-router-dom'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import { userRegister } from '../redux/actions/userAction'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import Spinner from '../components/Spinner'
+// ..
+AOS.init();
 
 function Register() {
     const dispatch = useDispatch() 
+    const {loading} = useSelector(state=>state.alertsReducer)
    function onFinish(values) {
     dispatch(userRegister(values))
     console.log(values)
@@ -14,9 +19,13 @@ function Register() {
 
     return(
         <div className='login'>
+             {loading && (<Spinner />)} 
           <Row gutter={16} className='d-flex align-items-center'>
                 <Col lg={16} style={{position:'relative'}}>
-                    <img src='https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?w=740&t=st=1670439611~exp=1670440211~hmac=f47db0d5996ac3591f0e7f6bc585cc8319af251e326a6eceb00c4e28957b4406' />
+                    <img 
+                    data-aos='slide-right'
+                    data-aos-duration='1500'
+                    src='https://img.freepik.com/free-vector/red-sedan-car-isolated-white-vector_53876-64366.jpg?w=740&t=st=1670439611~exp=1670440211~hmac=f47db0d5996ac3591f0e7f6bc585cc8319af251e326a6eceb00c4e28957b4406' />
                     <h1 className='login-logo'>GeorgeMotors</h1>
                 </Col>
                 <Col lg={8} className='text-left p-5'>
